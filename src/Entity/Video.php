d1c9fe8c64c171756ca8959b630e1de64aeb6a79
Lenovo P51 <chahid35.fc@gmail.com>
2023-11-01 13:43:20 +0100

    first commit

---
+++ src/Entity/Video.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\VideoRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: VideoRepository::class)]
+class Video
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $titre = null;
+
+    #[ORM\ManyToOne(inversedBy: 'videos')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Sondage $sondage = null;
+
+//    #[ORM\ManyToOne]
+//    #[ORM\JoinColumn(nullable: false)]
+//    private ?Sondage $sondages = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getSondage(): ?Sondage
+    {
+        return $this->sondage;
+    }
+
+    public function setSondage(?Sondage $sondage): self
+    {
+        $this->sondage = $sondage;
+
+        return $this;
+    }
+
+//    public function getSondages(): ?Sondage
+//    {
+//        return $this->sondages;
+//    }
+//
+//    public function setSondages(?Sondage $sondages): self
+//    {
+//        $this->sondages = $sondages;
+//
+//        return $this;
+//    }
+}
